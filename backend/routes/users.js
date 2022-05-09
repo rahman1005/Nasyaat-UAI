992889490cded866be9ea7a162ba3c969aaa2517
Aulia Rahman <75147892+rahman1005@users.noreply.github.com>
2022-05-09 08:25:37 +0700

    setup environment

---
+++ backend/routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+const res = require('express/lib/response');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send(process.env.APP_NAME);
+});
+
+router.get('/me', (req, res, next)=>{
+  res.send("helo mdf");
+});
+
+module.exports = router;
