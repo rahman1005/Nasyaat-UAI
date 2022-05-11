a691d533ae653cb3d1c6bf6da10a9a57aa5a98db
Aulia Rahman <75147892+rahman1005@users.noreply.github.com>
2022-05-11 15:35:34 +0700

    update navbar dashboard

--- frontend/src/components/NavbarDashboard.vue
+++ frontend/src/components/NavbarDashboard.vue
@@ -2,8 +2,9 @@
   <div class="nav-dashboard">
          <nav>
             <ul>
-                <li><a href="/dashboard">Event UAI</a></li>
-                <li><a href="/allevents">Event ORMAWA</a></li>
+                <li><a href="/dashboard">All Event</a></li>
+                <li><a href="">Event UAI</a></li>
+                <li><a href="">Event ORMAWA</a></li>
                 <li><a href="/about">Event UKKM</a></li>
                 <li ><button class="button-logout" >Log out</button></li>
                 
