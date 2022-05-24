ea44d44f713675b82657dfaf0706de2810a8bbfb
MirzaAliYafie18 <aliyafmirja@gmail.com>
2022-05-24 16:22:27 +0700

    Tambah Card All Events
    Menambahkan Card pada All Events

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,156 +1,28 @@
 <template>
-<div class="card-list">
-  <div class="card" style="width: 18rem">
-    <img
+  <div class="card  " style="width: 18rem">
+  <img
       class="card-img-top"
-      src="../assets/img/Hmif.png"
+      :src="'http://localhost:3000/'+event.Image"
       alt="Card image cap"
     />
     <div class="card-body">
-      <h5 class="card-title">Sonic UAI</h5>
+      <h5 class="card-title"> <router-link class="routerlink" :to="'/detailevent/' +event.eventId">{{event.nameEvent}}</router-link></h5>
       <p class="card-text">
         Penyelenggara :
-        HMIF UAI
+        {{event.lembagaName}}
       </p>
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item"><b-icon-pin-map icon="exclamation-circle-fill" variant="danger"></b-icon-pin-map> Zoom Meeting</li>
-      <li class="list-group-item"><b-icon-calendar icon="exclamation-circle-fill" variant="primary"></b-icon-calendar> 24 September 2021</li>
+      <li class="list-group-item"><b-icon-pin-map icon="exclamation-circle-fill" variant="danger"></b-icon-pin-map>  {{event.tempat}}</li>
+      <li class="list-group-item"><b-icon-calendar icon="exclamation-circle-fill" variant="primary"></b-icon-calendar> {{event.tanggal}}</li>
     </ul>
   </div>
-  <div class="card" style="width: 18rem">
-    <img
-      class="card-img-top"
-      src="../assets/img/FSTFAIR.jpg"
-      alt="Card image cap"
-    />
-    <div class="card-body">
-      <h5 class="card-title">FST FAIR</h5>
-      <p class="card-text">
-        Penyelenggara :
-        BEM FST UAI
-      </p>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item"><b-icon-pin-map icon="exclamation-circle-fill" variant="danger"></b-icon-pin-map> Zoom Meeting</li>
-      <li class="list-group-item"><b-icon-calendar icon="exclamation-circle-fill" variant="primary"></b-icon-calendar> 15 Oktober 2021</li>
-    </ul>
-  </div>
-    <div class="card" style="width: 18rem">
-    <img
-      class="card-img-top"
-      src="../assets/img/OceanBio.png"
-      alt="Card image cap"
-    />
-    <div class="card-body">
-      <h5 class="card-title">Ocean Talks</h5>
-      <p class="card-text">
-        Penyelenggara :
-        National Oceanographic
-      </p>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item"><b-icon-pin-map icon="exclamation-circle-fill" variant="danger"></b-icon-pin-map> Zoom Meeting</li>
-      <li class="list-group-item"><b-icon-calendar icon="exclamation-circle-fill" variant="primary"></b-icon-calendar> 22 Januari 2022</li>
-    </ul>
-  </div>
-  <div class="card" style="width: 18rem">
-    <img
-      class="card-img-top"
-      src="../assets/img/Himazi.png"
-      alt="Card image cap"
-    />
-    <div class="card-body">
-      <h5 class="card-title">HIMAZI'S CHARITY (MACHA)</h5>
-      <p class="card-text">
-        Penyelenggara :
-        HIMAZI UAI
-      </p>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item"><b-icon-pin-map icon="exclamation-circle-fill" variant="danger"></b-icon-pin-map> Taman Baca Carita,
-      Jalan Timbul Jaya No. 26 RT 006 RW 005, Kel. Duri Kosambi, Kec. Cengkareng, Kota Jakarta Barat, Prov. DKI Jakarta, 11750</li>
-      <li class="list-group-item"><b-icon-calendar icon="exclamation-circle-fill" variant="primary"></b-icon-calendar> 15 Oktober 2021</li>
-    </ul>
-  </div>
-  <div class="card" style="width: 18rem">
-    <img
-      class="card-img-top"
-      src="../assets/img/tes.png"
-      alt="Card image cap"
-    />
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </p>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">Cras justo odio</li>
-      <li class="list-group-item">Dapibus ac facilisis in</li>
-    </ul>
-  </div>
-  <div class="card" style="width: 18rem">
-    <img
-      class="card-img-top"
-      src="../assets/img/tes.png"
-      alt="Card image cap"
-    />
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </p>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">Cras justo odio</li>
-      <li class="list-group-item">Dapibus ac facilisis in</li>
-    </ul>
-  </div>
-  <div class="card" style="width: 18rem">
-    <img
-      class="card-img-top"
-      src="../assets/img/tes.png"
-      alt="Card image cap"
-    />
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </p>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">Cras justo odio</li>
-      <li class="list-group-item">Dapibus ac facilisis in</li>
-    </ul>
-  </div>
-  <div class="card" style="width: 18rem">
-    <img
-      class="card-img-top"
-      src="../assets/img/tes.png"
-      alt="Card image cap"
-    />
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </p>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">Cras justo odio</li>
-      <li class="list-group-item">Dapibus ac facilisis in</li>
-    </ul>
-  </div>
-</div>
 </template>
 
 <script>
 export default {
   name: "All-Events",
+  props: ["event"],
 };
 </script>
 
