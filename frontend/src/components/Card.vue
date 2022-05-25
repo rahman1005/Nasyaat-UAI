ebd4b572379ca535712194778014a6d40dea3fb8
Aulia Rahman <75147892+rahman1005@users.noreply.github.com>
2022-05-25 07:31:04 +0700

    update card-body

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -7,13 +7,10 @@
     />
     <div class="card-body">
       <h5 class="card-title"> <router-link class="routerlink" :to="'/detailevent/' +event.eventId">{{event.nameEvent}}</router-link></h5>
-      <p class="card-text">
-        Penyelenggara :
-        {{event.lembagaName}}
-      </p>
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item"><b-icon-pin-map icon="exclamation-circle-fill" variant="danger"></b-icon-pin-map>  {{event.tempat}}</li>
+      <li class="list-group-item">Penyelenggara :{{event.lembagaName}}</li>
+       <li class="list-group-item"><b-icon-pin-map icon="exclamation-circle-fill" variant="danger"></b-icon-pin-map>  {{event.tempat}}</li>
       <li class="list-group-item"><b-icon-calendar icon="exclamation-circle-fill" variant="primary"></b-icon-calendar> {{event.tanggal}}</li>
     </ul>
   </div>
