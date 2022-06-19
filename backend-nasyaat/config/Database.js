0c2eda3a1d61b5da83049c8d140975404af6d74d
Aulia Rahman <75147892+rahman1005@users.noreply.github.com>
2022-06-19 14:23:16 +0700

    ubah koneksi database

--- backend-nasyaat/config/Database.js
+++ backend-nasyaat/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
-const db = new Sequelize('nasyaat', 'root','',{
-    host:"localhost",
+const db = new Sequelize('u1713826_nasyaat-uai', 'u1713826_nasyaat-uai','h11tdx2wbm7l',{
+    host:"labifuai.com",
     dialect: "mysql"
 });
 export default db;
